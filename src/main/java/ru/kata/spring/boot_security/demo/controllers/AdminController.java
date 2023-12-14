217105df04147c71744626b13c885f1904953d95
TheTzdDark <timur.saitbattalow@yandex.ru>
2023-12-14 18:50:24 +0500

    Половина 3.1.4

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
@@ -1,6 +1,7 @@
 package ru.kata.spring.boot_security.demo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -21,8 +22,10 @@ public class AdminController {
     }
 
     @GetMapping
-    public String printAll(Model model) {
-        model.addAttribute("users", service.getAll());
+    public String printAll(@ModelAttribute("user") User user, Model model) {
+        model.addAttribute("allUsers", service.getAll());
+        model.addAttribute("allRoles", roleService.getAll());
+        model.addAttribute("currentUser", (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal());
         return "admin/index";
     }
 
